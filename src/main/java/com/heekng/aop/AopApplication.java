bc6b39ca3b2144de913adeee621db0a402da0695
heekng <rhkd1769@gmail.com>
2022-08-18 22:34:16 +0900

    포인트컷 참조

--- src/main/java/com/heekng/aop/AopApplication.java
+++ src/main/java/com/heekng/aop/AopApplication.java
@@ -3,6 +3,7 @@ package com.heekng.aop;
 import com.heekng.aop.order.aop.AspectV1;
 import com.heekng.aop.order.aop.AspectV2;
 import com.heekng.aop.order.aop.AspectV3;
+import com.heekng.aop.order.aop.AspectV4Pointcut;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 //@Import(AspectV1.class)
 //@Import(AspectV2.class)
-@Import(AspectV3.class)
+//@Import(AspectV3.class)
+@Import(AspectV4Pointcut.class)
 public class AopApplication {
 
 	public static void main(String[] args) {
