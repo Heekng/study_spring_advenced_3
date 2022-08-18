0a5ee5bc7110754c2e925f32661caf4c7cd7b484
heekng <rhkd1769@gmail.com>
2022-08-18 22:13:49 +0900

    포인트컷 분리

--- src/main/java/com/heekng/aop/AopApplication.java
+++ src/main/java/com/heekng/aop/AopApplication.java
@@ -1,12 +1,14 @@
 package com.heekng.aop;
 
 import com.heekng.aop.order.aop.AspectV1;
+import com.heekng.aop.order.aop.AspectV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(AspectV1.class)
+//@Import(AspectV1.class)
+@Import(AspectV2.class)
 public class AopApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/heekng/aop/order/aop/AspectV2.java
@@ -0,0 +1,22 @@
+package com.heekng.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV2 {
+
+    // com.heekng.aop.order 패키지와 하위 패키지
+    @Pointcut("execution(* com.heekng.aop.order..*(..))")
+    private void allOrder(){} // pointcut signature
+
+    @Around("allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("[log] {}", joinPoint.getSignature()); // join point 시그니처
+        return joinPoint.proceed();
+    }
+}
