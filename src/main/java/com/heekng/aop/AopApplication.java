600fe9cc81d336f702cd853f14a5e90c73a8db57
heekng <rhkd1769@gmail.com>
2022-08-18 22:10:24 +0900

    aop 적용

--- src/main/java/com/heekng/aop/AopApplication.java
+++ src/main/java/com/heekng/aop/AopApplication.java
@@ -1,9 +1,12 @@
 package com.heekng.aop;
 
+import com.heekng.aop.order.aop.AspectV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(AspectV1.class)
 public class AopApplication {
 
 	public static void main(String[] args) {
