030219c391d0aef8e8e11eb860030b2050d12523
heekng <rhkd1769@gmail.com>
2022-08-18 22:30:42 +0900

    어드바이스 추가

--- src/main/java/com/heekng/aop/AopApplication.java
+++ src/main/java/com/heekng/aop/AopApplication.java
@@ -2,13 +2,15 @@ package com.heekng.aop;
 
 import com.heekng.aop.order.aop.AspectV1;
 import com.heekng.aop.order.aop.AspectV2;
+import com.heekng.aop.order.aop.AspectV3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 //@Import(AspectV1.class)
-@Import(AspectV2.class)
+//@Import(AspectV2.class)
+@Import(AspectV3.class)
 public class AopApplication {
 
 	public static void main(String[] args) {
