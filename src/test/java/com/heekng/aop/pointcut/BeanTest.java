6113330f62aea82dd2d600136ff804b326e41074
heekng <rhkd1769@gmail.com>
2022-08-19 16:03:18 +0900

    Bean 지시자

---
+++ src/test/java/com/heekng/aop/pointcut/BeanTest.java
@@ -0,0 +1,38 @@
+package com.heekng.aop.pointcut;
+
+import com.heekng.aop.member.MemberService;
+import com.heekng.aop.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootTest
+@Import(BeanTest.BeanAspect.class)
+public class BeanTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Test
+    void success() {
+        log.info("orderService Proxy={}", orderService.getClass());
+        orderService.orderItem("itemA");
+    }
+
+    @Aspect
+    @Slf4j
+    static class BeanAspect {
+        @Around("bean(orderService) || bean(*Repository)")
+        public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[bean] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+
+}
