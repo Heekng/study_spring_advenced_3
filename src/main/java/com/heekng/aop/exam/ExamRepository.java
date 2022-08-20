d0b9891a75414376d03070b18473bfc24caabfd6
heekng <rhkd1769@gmail.com>
2022-08-20 16:15:15 +0900

    로그 출력 AOP 예제'

---
+++ src/main/java/com/heekng/aop/exam/ExamRepository.java
@@ -0,0 +1,24 @@
+package com.heekng.aop.exam;
+
+import com.heekng.aop.exam.annotation.Trace;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ExamRepository {
+
+    private static int seq = 0;
+
+    /**
+     * 5번에 1번 실패하는 요청
+     * @param itemId
+     * @return
+     */
+    @Trace
+    public String save(String itemId) {
+        seq++;
+        if (seq % 5 == 0) {
+            throw new IllegalStateException("예외 발생");
+        }
+        return "ok";
+    }
+}
